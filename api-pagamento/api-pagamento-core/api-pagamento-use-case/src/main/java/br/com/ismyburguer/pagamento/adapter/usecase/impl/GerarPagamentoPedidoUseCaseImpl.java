b53785766fc694c546e0205a4dc81e7040642d28
Ismael G Costa <notmymail@notmymail.com>
2024-08-02 01:29:01 -0300

    finalizando implementação da saga

--- api-pagamento/api-pagamento-core/api-pagamento-use-case/src/main/java/br/com/ismyburguer/pagamento/adapter/usecase/impl/GerarPagamentoPedidoUseCaseImpl.java
+++ api-pagamento/api-pagamento-core/api-pagamento-use-case/src/main/java/br/com/ismyburguer/pagamento/adapter/usecase/impl/GerarPagamentoPedidoUseCaseImpl.java
@@ -27,4 +27,5 @@ public class GerarPagamentoPedidoUseCaseImpl implements GerarPagamentoPedidoUseC
         pagamento.setQrCode("00020101021243650016COM.MERCADOLIBRE02013063638f1192a-5fd1-4180-a180-8bcae3556bc35204000053039865802BR5925IZABEL AAAA DE MELO6007BARUERI62070503***63040B6D");
         return pagamentoRepository.salvar(pagamento);
     }
+
 }
